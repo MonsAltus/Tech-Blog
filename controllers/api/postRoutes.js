553da61ec528fbb8d94e058f535a578f5fc7abb7
James <James.Hacklander@gmail.com>
2021-06-12 23:20:53 -0700

    fixed typos. Developed api postRoutes and commentRoutes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,2 +1,9 @@
 const router = require('express').Router();
 const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// Post -- (withAuth)
+
+// Delete by id  -- (withAuth)
+
+module.exports = router;
