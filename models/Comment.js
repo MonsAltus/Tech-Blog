602d335653eb06c32af04be6ff2c9b70894a391a
James <James.Hacklander@gmail.com>
2021-06-12 22:14:24 -0700

    Developed Comment model

--- models/Comment.js
+++ models/Comment.js
@@ -1,19 +1,15 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model {}
+class Comment extends Model {}
 
-Post.init(
+Comment.init(
     {
         id: {
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
             autoIncrement: true,
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
         },
         content: {
             type: DataTypes.STRING,
@@ -24,6 +20,13 @@ Post.init(
             allowNull: false,
             defaultValue: DataTypes.NOW,
         },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id'
+            },
+        },
         user_id: {
             type: DataTypes.INTEGER,
             references: {
@@ -31,15 +34,14 @@ Post.init(
                 key: 'id'
             },
         },
-        // Need comments ref?
     },
     {
         sequelize,
         timestamps: false,
         freezeTableName: true,
         underscored: true,
-        modelName: 'post',
+        modelName: 'user',
     }
 );
 
-module.exports = Post;
+module.exports = Comment;
