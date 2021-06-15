432f475f8ebdbb9d5479bdbef71c99fd1c3764eb
James <James.Hacklander@gmail.com>
2021-06-15 11:58:45 -0700

    Developed Comment and Post api routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,8 +2,36 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-// Post -- (withAuth)
+// Create new Comment, requires user to be logged in.
+router.post('/', withAuth, async (req, res) => {
+    try{
+        const newComment = await Comment.create({
+            ...req.body,
+            user_id: req.session.user_id
+        });
+        res.status(200).json(newComment)
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
-// Delete by id  -- (withAuth)
+// Delete Comment by by id, requires user to be logged in.
+router.delete('/:id', withAuth, async (req, res) => {
+    try{
+        const commentData = await Comment.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            }
+        });
+        if(!commentData) {
+            res.status(404).json({ message: 'No comment found with this id.'});
+            return;
+        }
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 module.exports = router;
