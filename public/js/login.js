5b7124a2b557d6ac4d7c7263da93acdfac400461
James <James.Hacklander@gmail.com>
2021-06-16 17:28:07 -0700

    fixed href error in homepage.handlebars. Developed Login/signup and logout scripts in /public/

---
+++ public/js/login.js
@@ -0,0 +1,52 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        // If both fields have data use login api route.
+        const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+        });
+        if (response.ok) {
+            // If successful, redirect to homepage.
+            document.location.replace('/homepage');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (name && email && password) {
+        // If all fields have data use POST users api route.
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        if (response.ok) {
+            // If successful, redirect to homepage.
+            document.location.replace('/homepage');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+
+document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
